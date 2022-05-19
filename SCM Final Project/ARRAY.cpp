83514770665fedf03905027b6f84069a89d6577c
anirudh7664 <anirudh0190.be21@chitkara.edu.in>
2022-05-19 22:08:43 +0530

    3 codes added

---
+++ SCM Final Project/ARRAY.cpp
@@ -0,0 +1,84 @@
+//Arrays- Store multiple values with a single name.
+
+//1.
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int a[3]={10,20,30};
+//	cout<<a[0]<<a[1]<<a[2];
+//	cout<<sizeof(a);
+//  	
+//}
+
+
+//2.//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int a[3]={10,20,30};
+//	for(int i=0;i<=2;i++)
+//	{
+//		cout<<a[i];
+//	}
+//}
+
+//3.
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int a[5];
+//	for (int i=0; i<=4;i++)
+//	cin>>a[i];
+//	cout<<a[0]<<" "<<a[1];
+//	
+//}
+
+//4. //2D ARRAY
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	string l[2][4]
+//	= {
+//		{"a","b","c","d"};
+//		{"e","f","g","h"};
+//		
+//		
+//		
+//		
+//	}
+//	
+//}
+
+//5. 3D
+#include <iostream>
+using namespace std;
+int main()
+{
+	string l[2][2][2]={{
+	{"a","b"},
+	{"c","d"}
+	
+	},
+	{
+	{"e","f"},
+	{"g","h"}
+	
+	
+	}
+	};
+	for(int i=0;i<2;i++)
+	{
+		for(int j=0;j<2;j++)
+		
+	}
+	{for(int k=0;k<2;k++)}
+	{
+	cout<<l[i][j][k]<<endl;
+	}
+	
+	
+	
+}
